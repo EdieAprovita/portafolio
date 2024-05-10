27109c70db4cb039e77310084e051a287902af67
Edgar Chaver <edieveg316@gmail.com>
2024-05-10 16:48:39 -0600

    Update HeroSection.tsx

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -64,6 +64,13 @@ const HeroSection = () => {
 								Download CV
 							</span>
 						</Link>
+						<Link
+							href="https://github.com/EdieAprovita"
+							className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
+							<span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+								Github
+							</span>
+						</Link>
 					</div>
 				</motion.div>
 				<motion.div
