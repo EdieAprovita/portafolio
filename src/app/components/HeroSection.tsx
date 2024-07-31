67d0c0514aef846542e5324798b98233997e4ddc
Edgar Chaver <edieveg316@gmail.com>
2024-07-30 19:53:04 -0600

    Update HeroSection.tsx

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -15,21 +15,23 @@ const HeroSection = () => {
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{ duration: 0.5 }}
 					className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
-					<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+					<h1 className="text-white mb-4 text-xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
 						<span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-							Hello, I&apos;m{" "}
+							Hello, I&apos;m{" Edgar Chavero "}
 						</span>
 						<br />
 						<TypeAnimation
 							sequence={[
 								"Edgar Chavero",
-								1000,
+								2000,
 								"Frontend",
-								1000,
+								2000,
 								"Backend",
-								1000,
+								2000,
 								"React / Node",
-								1000,
+								2500,
+								"Fullstack",
+								2500,
 							]}
 							wrapper="span"
 							speed={50}
@@ -81,8 +83,8 @@ const HeroSection = () => {
 							src="/images/image.png"
 							alt="hero image"
 							className=" rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-							width={300}
-							height={300}
+							width={400}
+							height={400}
 						/>
 					</div>
 				</motion.div>
